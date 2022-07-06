eb9894ffd154763ffb0b2093785cef55d1cfb9f1
thanhnd1997 <thanhnguyen.281297.dl@gmail.com>
2022-07-06 15:55:47 +0700

    asfasf

--- scripts/dockercheck.sh
+++ scripts/dockercheck.sh
@@ -3,7 +3,7 @@ CONTAINER_NAME="nginx"
 checkContainer(){
     EXISTS_NAME_CONTAINER=$(docker inspect --type=container --format="{{.Name}}" $CONTAINER_NAME)
     if [ $( docker ps -a -f name=$CONTAINER_NAME | wc -l ) -eq 2 ]; then
-        docker exec -it $CONTAINER_NAME /bin/bash
+        docker exec -it $CONTAINER_NAME /bin/bash -c ls -l
     else
         docker run -it -d -p 8081:80 --name $CONTAINER_NAME nginx:latest
     fi
