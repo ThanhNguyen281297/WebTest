228706126ccd42f6cb70e657b0b04376b6993b5e
thanhnd1997 <thanhnguyen.281297.dl@gmail.com>
2022-07-06 16:30:42 +0700

    asfasf

--- scripts/dockercheck.sh
+++ scripts/dockercheck.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 WORKSPACE="/home/docker/workspace/Pipeline/"
 CONTAINER_NAME="nginx"
-checkContainer(){
-    EXISTS_NAME_CONTAINER=$(docker inspect --type=container --format="{{.Name}}" $CONTAINER_NAME)
+# checkContainer(){
+#     EXISTS_NAME_CONTAINER=$(docker inspect --type=container --format="{{.Name}}" $CONTAINER_NAME)
+#     if [ $( docker ps -a -f name=$CONTAINER_NAME | wc -l ) -eq 2 ]; then
+#         docker cp -a $WORKSPACE/index.html ${CONTAINER_NAME}:/usr/share/nginx/html/
+#     else
+#         docker pull nginx
+#         docker run -it -d -p 8081:80 --name $CONTAINER_NAME nginx:latest
+#     fi
+# }
+# checkContainer
+check_exist() {
+    EXISTING_IMAGE_ID=$(docker inspect --type=container --format="{{.Image}}" $CONTAINER_NAME)
     if [[ "$EXISTING_IMAGE_ID" == "null" ]]; then
         docker cp -a $WORKSPACE/index.html ${CONTAINER_NAME}:/usr/share/nginx/html/
     else
@@ -10,4 +20,3 @@ checkContainer(){
         docker run -it -d -p 8081:80 --name $CONTAINER_NAME nginx:latest
     fi
 }
-checkContainer
