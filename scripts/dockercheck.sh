47a87098058f6ab49756b2062a717ac3355a72bf
thanhnd1997 <thanhnguyen.281297.dl@gmail.com>
2022-07-06 13:51:46 +0700

    asfasf

--- scripts/dockercheck.sh
+++ scripts/dockercheck.sh
@@ -2,7 +2,12 @@
 checkContainerID(){
     docker ps -a >> listContainer.txt
     containerID=$(awk '{print $1}' listContainer.txt)
-    echo $containerID
+    if [$containerID]
+    then
+        echo $containerID
+    else
+        docker pull nginx
+        docker run -it -d -p 8081:80 nginx:latest 
+    fi
 }
-
 checkContainerID
