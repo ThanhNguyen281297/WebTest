52a5fa6686da2d0042d93ffb4be19c4d9744387e
thanhnd1997 <thanhnguyen.281297.dl@gmail.com>
2022-07-06 16:23:11 +0700

    asfasf

--- scripts/dockercheck.sh
+++ scripts/dockercheck.sh
@@ -3,8 +3,7 @@ WORKSPACE="/home/docker/workspace/Pipeline/"
 CONTAINER_NAME="nginx"
 checkContainer(){
     EXISTS_NAME_CONTAINER=$(docker inspect --type=container --format="{{.Name}}" $CONTAINER_NAME)
-    # if [ $( docker ps -a -f name=$CONTAINER_NAME | wc -l ) -eq 2 ]; then
-    if [["$CONTAINER_NAME" == "null"]]; then
+    if [ $( docker ps -a -f name=$CONTAINER_NAME | wc -l ) -eq 2 ]; then
         docker cp -a $WORKSPACE/index.html ${CONTAINER_NAME}:/usr/share/nginx/html/
     else
         docker pull nginx
