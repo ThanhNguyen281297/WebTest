e074668f3d71dcb6cca1e45f3541e0bb6d40696b
thanhnd1997 <thanhnguyen.281297.dl@gmail.com>
2022-07-07 09:40:05 +0700

    123

--- scripts/dockercheck.sh
+++ scripts/dockercheck.sh
@@ -4,7 +4,7 @@ CONTAINER_NAME="nginx"
 checkContainer(){
     EXISTS_NAME_CONTAINER=$(docker inspect --type=container --format="{{.Name}}" $CONTAINER_NAME)
     if [ $( docker ps -a -f name=$CONTAINER_NAME | wc -l ) -eq 2 ]; then
-        # docker cp -a $WORKSPACE/index.html ${CONTAINER_NAME}:/usr/share/nginx/html/
+        docker cp -a $WORKSPACE/index.html ${CONTAINER_NAME}:/usr/share/nginx/html/
     else
         docker pull nginx
         docker run -it -d -p 8081:80 --name $CONTAINER_NAME --volume "nginx-config":/etc/nginx/conf.d nginx:latest
