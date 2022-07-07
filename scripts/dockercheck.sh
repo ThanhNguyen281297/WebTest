0de5f6dd87fc91b7eb5eed4e374d05e760cbd274
thanhnd1997 <thanhnguyen.281297.dl@gmail.com>
2022-07-07 11:48:35 +0700

    123

--- scripts/dockercheck.sh
+++ scripts/dockercheck.sh
@@ -7,9 +7,9 @@ checkContainer(){
         docker cp -a $WORKSPACE/ ${CONTAINER_NAME}:/usr/share/nginx/html/
     else
         docker pull nginx
-        docker run -it -d -p 8081:80 --name $CONTAINER_NAME  nginx:latest
+        docker run -it -d -p 8081:80 --name $CONTAINER_NAME nginx:latest
         docker cp -a $WORKSPACE/ ${CONTAINER_NAME}:/usr/share/nginx/html/
-        docker exec $CONTAINER_NAME /bin/bash -c sed -i 's|/usr/share/nginx/html|/usr/share/nginx/html/Pipeline|g' /etc/nginx/conf.d/default.conf
+        docker exec $CONTAINER_NAME /bin/bash -c "sed -i 's|/usr/share/nginx/html|/usr/share/nginx/html/Pipeline|g' /etc/nginx/conf.d/default.conf"
     fi
 }
 checkContainer
