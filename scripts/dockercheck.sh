0aacb2c6ea2edd260ec2e90ba747911d646b5082
thanhnd1997 <thanhnguyen.281297.dl@gmail.com>
2022-07-07 13:42:25 +0700

    123

--- scripts/dockercheck.sh
+++ scripts/dockercheck.sh
@@ -5,11 +5,12 @@ checkContainer(){
     EXISTS_NAME_CONTAINER=$(docker inspect --type=container --format="{{.Name}}" $CONTAINER_NAME)
     if [ $( docker ps -a -f name=$CONTAINER_NAME | wc -l ) -eq 2 ]; then
         docker cp -a $WORKSPACE/ ${CONTAINER_NAME}:/usr/share/nginx/html/
+        docker exec $CONTAINER_NAME /bin/bash -c "service nginx reload" 
     else
         docker pull nginx
         docker run -it -d -p 8081:80 --name $CONTAINER_NAME nginx:latest
         docker cp -a $WORKSPACE/ ${CONTAINER_NAME}:/usr/share/nginx/html/
-        docker exec $CONTAINER_NAME /bin/bash -c "sed -i 's|/usr/share/nginx/html|/usr/share/nginx/html/Pipeline/|g' /etc/nginx/conf.d/default.conf"
+        docker exec $CONTAINER_NAME /bin/bash -c "sed -i 's|/usr/share/nginx/html|/usr/share/nginx/html/Pipeline|g' /etc/nginx/conf.d/default.conf"
         docker exec $CONTAINER_NAME /bin/bash -c "service nginx reload" 
     fi
 }
